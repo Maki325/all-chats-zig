4816fd11ac92dc9b9453a0370ffd4ed67b0fa98d
Marko <markoni521@gmail.com>
2024-06-13 21:19:39 +0200

    Cleaned up build script

--- build.zig
+++ build.zig
@@ -1,8 +1,60 @@
 const std = @import("std");
 
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const protocol = NamedModule.fromFile(b, "protocol", "./src/protocol/lib.zig");
+    const dotenv = NamedModule.fromFile(b, "dotenv", "./deps/dotenv/lib.zig");
+    const websocket = NamedModule.fromZon(b, "websocket");
+    const httpz = NamedModule.fromZon(b, "httpz");
+
+    const server = addModules(b.addExecutable(.{
+        .name = "combining-chats",
+        .root_source_file = b.path("src/server/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    }), &.{ protocol, httpz, dotenv });
+    addSQLite(b, server);
+    b.installArtifact(server);
+    addRunStep(b, server, "run-server", false);
+
+    const bot_twitch = addModules(b.addExecutable(.{
+        .name = "bot-twitch",
+        .root_source_file = b.path("src/twitch/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    }), &.{ protocol, websocket, dotenv });
+    b.installArtifact(bot_twitch);
+    addRunStep(b, bot_twitch, "run-twitch", false);
+
+    const bot_youtube = addModules(b.addExecutable(.{
+        .name = "bot-youtube",
+        .root_source_file = b.path("src/youtube/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    }), &.{ protocol, websocket });
+    b.installArtifact(bot_youtube);
+    addRunStep(b, bot_youtube, "run-youtube", true);
+}
+
 const NamedModule = struct {
     name: []const u8,
     module: *std.Build.Module,
+
+    fn fromFile(b: *std.Build, name: []const u8, path: []const u8) NamedModule {
+        return .{
+            .name = name,
+            .module = b.addModule(name, .{ .root_source_file = .{ .path = path } }),
+        };
+    }
+
+    fn fromZon(b: *std.Build, name: []const u8) NamedModule {
+        return .{
+            .name = name,
+            .module = b.dependency(name, .{}).module(name),
+        };
+    }
 };
 
 pub fn addModules(exe: *std.Build.Step.Compile, modules: []const NamedModule) *std.Build.Step.Compile {
@@ -16,70 +68,24 @@ pub fn addModules(exe: *std.Build.Step.Compile, modules: []const NamedModule) *s
     return exe;
 }
 
-pub fn build(b: *std.Build) void {
-    const protocol = NamedModule{
-        .name = "protocol",
-        .module = b.addModule("protocol", .{ .root_source_file = .{ .path = "./src/protocol/lib.zig" } }),
-    };
-    const websocket = NamedModule{
-        .name = "websocket",
-        .module = b.dependency("websocket", .{}).module("websocket"),
-    };
-    const httpz = NamedModule{
-        .name = "httpz",
-        .module = b.dependency("httpz", .{}).module("httpz"),
-    };
-    const dotenv = NamedModule{
-        .name = "dotenv",
-        .module = b.addModule("dotenv", .{ .root_source_file = .{ .path = "./deps/dotenv/lib.zig" } }),
-    };
-
-    const server = addModules(b.addExecutable(.{
-        .name = "combining-chats",
-        .root_source_file = b.path("src/server/main.zig"),
-        .target = b.host,
-    }), &.{ protocol, httpz, dotenv });
-    {
-        const sqlite = b.dependency("sqlite", .{
-            .target = b.host,
-        });
-
-        server.root_module.addImport("sqlite", sqlite.module("sqlite"));
-
-        // links the bundled sqlite3, so leave this out if you link the system one
-        server.linkLibrary(sqlite.artifact("sqlite"));
-    }
-    b.installArtifact(server);
-    {
-        const run_server = b.addRunArtifact(server);
-        const run_server_step = b.step("run-server", "Run the application");
-        run_server_step.dependOn(&run_server.step);
-    }
-
-    const bot_twitch = addModules(b.addExecutable(.{
-        .name = "bot-twitch",
-        .root_source_file = b.path("src/twitch/main.zig"),
-        .target = b.host,
-    }), &.{ protocol, websocket, dotenv });
-    b.installArtifact(bot_twitch);
-    {
-        const run_twitch = b.addRunArtifact(bot_twitch);
-        const run_twitch_step = b.step("run-twitch", "Run the application");
-        run_twitch_step.dependOn(&run_twitch.step);
-    }
-
-    const bot_youtube = addModules(b.addExecutable(.{
-        .name = "bot-youtube",
-        .root_source_file = b.path("src/youtube/main.zig"),
-        .target = b.host,
-    }), &.{ protocol, websocket });
-    b.installArtifact(bot_youtube);
-    {
-        const run_youtube = b.addRunArtifact(bot_youtube);
+pub fn addRunStep(b: *std.Build, exe: *std.Build.Step.Compile, name: []const u8, pass_args: bool) void {
+    const run = b.addRunArtifact(exe);
+    if (pass_args) {
         if (b.args) |args| {
-            run_youtube.addArgs(args);
+            run.addArgs(args);
         }
-        const run_youtube_step = b.step("run-youtube", "Run the application");
-        run_youtube_step.dependOn(&run_youtube.step);
     }
+    const run_step = b.step(name, "Run the application");
+    run_step.dependOn(&run.step);
+}
+
+pub fn addSQLite(b: *std.Build, exe: *std.Build.Step.Compile) void {
+    const sqlite = b.dependency("sqlite", .{
+        .target = b.host,
+    });
+
+    exe.root_module.addImport("sqlite", sqlite.module("sqlite"));
+
+    // link the bundled sqlite3
+    exe.linkLibrary(sqlite.artifact("sqlite"));
 }
